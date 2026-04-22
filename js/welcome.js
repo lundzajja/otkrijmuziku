4dff1ea56bd0c01d41114ce39ef26fd2f4e09379
lundzajja <luka.matic.25@vipos.edu.rs>
2026-04-22 09:13:52 +0200

    1

--- js/welcome.js
+++ js/welcome.js
@@ -10,33 +10,9 @@
     return;
   }
 
-  var PLAY_DURATION = 60;
-
-  function startAudio() {
-    var audio = new Audio('./a-g-cook-residue.m4a');
-    audio.volume = 0.18;
-    audio.play().catch(function (err) {
-      console.warn('Audio nije mogao da se pusti:', err);
-    });
-
-    var startTime = Date.now();
-    var ticker = setInterval(function () {
-      var elapsed  = (Date.now() - startTime) / 1000;
-      var progress = elapsed / PLAY_DURATION;
-      if (progress >= 1) {
-        audio.volume = 0;
-        audio.pause();
-        clearInterval(ticker);
-        return;
-      }
-      audio.volume = 0.18 * (1 - progress);
-    }, 200);
-  }
-
   function dismissOverlay() {
     sessionStorage.setItem('otkrijzvuk_visited', '1');
     overlay.classList.add('is-hiding');
-    startAudio();
 
     overlay.addEventListener('transitionend', function () {
       overlay.classList.add('is-hidden');
